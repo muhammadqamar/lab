36160a0b17ca576a8ad52e0ed9033aa0750cb227
muhammadqamar <muhammadqamar111@gmail.com>
2023-02-17 20:09:09 +0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Error from "./pages/error";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
+      {/* <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} /> */}
+    </Routes>
+  );
 };
 
 export default App;
